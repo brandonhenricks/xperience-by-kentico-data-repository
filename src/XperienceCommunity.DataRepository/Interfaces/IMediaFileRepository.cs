aa9569c20388e2219a7acd90d172113b35072b00
Brandon Henricks <brandon.henricks1984@gmail.com>
2025-01-08 16:11:24 -0500

    Add IMediaFileRepository and implement caching logic
    Introduced IMediaFileRepository interface with async methods for retrieving media files. Added MediaFileRepository class implementing the interface, utilizing IProgressiveCache and ObjectQuery<MediaFileInfo> for caching and querying. Updated DependencyInjection to register the new service with TryAddScoped.

---
+++ src/XperienceCommunity.DataRepository/Interfaces/IMediaFileRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+
+using CMS.MediaLibrary;
+
+namespace XperienceCommunity.DataRepository.Interfaces;
+
+/// <summary>
+/// Interface for media file repository to handle media file operations.
+/// </summary>
+public interface IMediaFileRepository
+{
+    /// <summary>
+    /// Retrieves a list of media files based on the provided GUIDs.
+    /// </summary>
+    /// <param name="mediaFileGuids">The GUIDs of the media files to retrieve.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains an immutable list of <see cref="MediaFileInfo"/>.</returns>
+    Task<ImmutableList<MediaFileInfo>> GetMediaFilesAsync(IEnumerable<Guid> mediaFileGuids,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves a list of media files based on the provided related items.
+    /// </summary>
+    /// <param name="items">The related items to retrieve media files from.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains an immutable list of <see cref="MediaFileInfo"/>.</returns>
+    Task<ImmutableList<MediaFileInfo>> GetAssetsFromRelatedItemsAsync(IEnumerable<AssetRelatedItem> items,
+        CancellationToken cancellationToken = default);
+}
